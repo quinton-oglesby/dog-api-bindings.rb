754ff51d0434586da1e1684f50e988868f76429b
Quinton Oglesby <quinton.oglesby@icloud.com>
2022-01-25 05:29:26 -0600

    updated function names + gemfile

--- lib/dog-api.rb
+++ lib/dog-api.rb
@@ -4,31 +4,31 @@ require 'http'
 
 API_ENDPOINT = 'https://dog.ceo/api'
 
-def return_breeds_list
+def breeds_list
   response = HTTP.get("#{API_ENDPOINT}/breeds/list/all")
   hashed_response = response.parse['message'] if response.code == 200
   hashed_response or "Could not connect to the API, Status Code: #{response.code}"
 end
 
-def return_sub_breeds_list(breed)
+def sub_breeds_list(breed)
   response = HTTP.get("#{API_ENDPOINT}/breed/#{breed}/list")
   hashed_response = response.parse['message'] if response.code == 200
   hashed_response || "Could not connect to the API, Status Code: #{response.code}. Perhaps this breed does not have any sub-breeds?"
 end
 
-def return_breed_images(breed)
+def all_breed_images(breed)
   response = HTTP.get("#{API_ENDPOINT}/breed/#{breed}/images")
   hashed_response = response.parse['message'] if response.code == 200
   hashed_response || "Could not connect to the API, Status Code: #{response.code}"
 end
 
-def return_sub_breed_images(breed, sub_breed)
+def all_sub_breed_images(breed, sub_breed)
   response = HTTP.get("#{API_ENDPOINT}/breed/#{breed}/#{sub_breed}/images")
   hashed_response = response.parse['message'] if response.code == 200
   hashed_response || "Could not connect to the API, Status Code: #{response.code} Perhaps this breed does not have any sub-breeds?"
 end
 
-def return_random_images(amount)
+def random_images(amount)
   if amount >= 1 && amount <= 50
     response = HTTP.get("#{API_ENDPOINT}/breeds/image/random/#{amount}")
     hashed_response = response.parse['message'] if response.code == 200
@@ -38,7 +38,7 @@ def return_random_images(amount)
   end
 end
 
-def return_random_breed_images(breed, amount)
+def random_breed_images(breed, amount)
   if amount >= 1 && amount <= 50
     response = HTTP.get("#{API_ENDPOINT}/breed/#{breed}/images/random/#{amount}")
     hashed_response = response.parse['message'] if response.code == 200
@@ -48,7 +48,7 @@ def return_random_breed_images(breed, amount)
   end
 end
 
-def return_random_sub_breed_images(breed, sub_breed, amount)
+def random_sub_breed_images(breed, sub_breed, amount)
   if amount >= 1 && amount <= 50
     response = HTTP.get("#{API_ENDPOINT}/breed/#{breed}/#{sub_breed}/images/random/#{amount}")
     hashed_response = response.parse['message'] if response.code == 200

65d8d12710f436b3293220b1633b7813d29b4488
Quinton Oglesby <quinton.oglesby@icloud.com>
2022-01-25 08:45:31 -0600

    updated rdoc

--- dogapi.gemspec
+++ dogapi.gemspec
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 Gem::Specification.new do |s|
   s.name        = 'dog_api_bindings'
   s.version     = '0.0.0'
   s.summary     = 'Ruby bindings for the dog.ceo API.'
-  s.description = 'rMy first ruby gem.'
+  s.description = 'My first ruby gem.'
   s.authors     = ['Quinton Oglesby']
   s.email       = 'qoglesby97@gmail.com'
   s.files       = ['lib/dog-api.rb']
   s.homepage    = 'https://rubygems.org/gems/dog_api_bindings'
   s.license     = 'MIT'
+
+  s.required_ruby_version = '>= 3.1.0'
+  s.add_runtime_dependency 'http'
 end
--- lib/dog-api.rb
+++ lib/dog-api.rb
@@ -4,30 +4,37 @@ require 'http'
 
 API_ENDPOINT = 'https://dog.ceo/api'
 
+# Returns a hash of all available breeds.
 def breeds_list
   response = HTTP.get("#{API_ENDPOINT}/breeds/list/all")
   hashed_response = response.parse['message'] if response.code == 200
   hashed_response or "Could not connect to the API, Status Code: #{response.code}"
 end
 
+# Returns a hash of all available sub-breeds of the given breed.
 def sub_breeds_list(breed)
   response = HTTP.get("#{API_ENDPOINT}/breed/#{breed}/list")
   hashed_response = response.parse['message'] if response.code == 200
   hashed_response || "Could not connect to the API, Status Code: #{response.code}. Perhaps this breed does not have any sub-breeds?"
 end
 
+# Returns an array of all available images of the given breed.
 def all_breed_images(breed)
   response = HTTP.get("#{API_ENDPOINT}/breed/#{breed}/images")
   hashed_response = response.parse['message'] if response.code == 200
   hashed_response || "Could not connect to the API, Status Code: #{response.code}"
 end
 
+# Returns an array of all available images of the given breed and sub-breed.
 def all_sub_breed_images(breed, sub_breed)
   response = HTTP.get("#{API_ENDPOINT}/breed/#{breed}/#{sub_breed}/images")
   hashed_response = response.parse['message'] if response.code == 200
   hashed_response || "Could not connect to the API, Status Code: #{response.code} Perhaps this breed does not have any sub-breeds?"
 end
 
+# Returns an array of random breed images equivalent to the passed amount.
+#
+# Due to API limitations, it can only return an amount between 1 and 50.
 def random_images(amount)
   if amount >= 1 && amount <= 50
     response = HTTP.get("#{API_ENDPOINT}/breeds/image/random/#{amount}")
@@ -38,6 +45,9 @@ def random_images(amount)
   end
 end
 
+# Returns an array of breed images equivalent to the passed amount.
+#
+# Due to API limitations, it can only return an amount between 1 and 50.
 def random_breed_images(breed, amount)
   if amount >= 1 && amount <= 50
     response = HTTP.get("#{API_ENDPOINT}/breed/#{breed}/images/random/#{amount}")
@@ -48,7 +58,7 @@ def random_breed_images(breed, amount)
   end
 end
 
-# Returns an amount of sub-breed images equivalent to the passed amount.
+# Returns an array of sub-breed images equivalent to the passed amount.
 #
 # Due to API limitations, it can only return an amount between 1 and 50.
 def random_sub_breed_images(breed, sub_breed, amount)
